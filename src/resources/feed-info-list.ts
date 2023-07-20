ff599406924c3c2c9fbb99fa003d674afd5a640f
Donghoon Yeo <angeloyeo@gmail.com>
2023-07-20 09:50:40 +0900

    Update feed-info-list.ts

--- src/resources/feed-info-list.ts
+++ src/resources/feed-info-list.ts
@@ -20,4 +20,5 @@ export const FEED_INFO_LIST: FeedInfo[] = createFeedInfoList([
   ['게으른 매트랩', 'https://lazymatlab.tistory.com/feed'],
   ['허밍스테레오', 'https://hummingstereo.tistory.com/feed'],
   ['MATLAB&Simulink 공식 블로그', 'https://rss.blog.naver.com/matlablove'],
+  ['MATLAB Korea 공식 유튜브 채널', 'https://www.youtube.com/feeds/videos.xml?channel_id=UC6Zjgg_0PQBm96aHeiXrjXQ'],
 ]);
