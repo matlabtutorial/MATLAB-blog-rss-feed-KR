161ae155be891f895a387c869ef9cb5c7f359824
Angelo Yeo <angeloyeo@gmail.com>
2025-03-28 13:11:41 +0900

    Update blogFeeds.js

--- src/site/_data/blogFeeds.js
+++ src/site/_data/blogFeeds.js
@@ -2,7 +2,7 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
-import 'dayjs/locale/ja';
+import 'dayjs/locale/ko';
 
 dayjs.extend(relativeTime);
 dayjs.extend(timezone);
